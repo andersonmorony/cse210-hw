22b69d006e7b8452b41fce4cb7470da779c13257
Anderson Moroni <andersonmoroni92@gmail.com>
2024-01-12 11:28:02 -0300

    feat: C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int inputUser;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.Write("Enter number: ");
+            inputUser = int.Parse(Console.ReadLine());
+            numbers.Add(inputUser);
+
+        } while (inputUser != 0);
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is:: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
