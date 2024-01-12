8b6589f400ee12286f67053611eb2704c7736bdd
Anderson Moroni <andersonmoroni92@gmail.com>
2024-01-12 09:34:10 -0300

    feat: task two - conditional

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = string.Empty;
+
+        // Verify the letter
+        if(grade >= 90)
+        {
+            letter = "A";
+        }
+        else if(grade >= 80 && grade < 90)
+        {
+           letter = "B";
+        }
+        else if(grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if(grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if(grade < 60)
+        {
+            letter = "F";
+        }
+
+        // Show message
+        if(grade >= 70)
+        {
+            Console.WriteLine($"Congratulation, your grade is {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Was almost there, try again, your grade is {letter}");
+        }
     }
 }
