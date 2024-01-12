58b979ebe03874f5192c044bf6492ab5f5cb2f27
Anderson Moroni <andersonmoroni92@gmail.com>
2024-01-12 10:34:46 -0300

    feat: C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int userGuess;
+        string playAgain = "yes";
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if(userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Do you want play again? [Yes] or [No]");
+                playAgain = Console.ReadLine().ToLower();
+                magicNumber = randomGenerator.Next(1, 100);
+
+            }
+
+        } while (playAgain == "yes");
+
     }
 }
