d3528a670be4bb9f05aecb6a34fd39e41994e408
Anderson Moroni <andersonmoroni92@gmail.com>
2024-01-15 18:47:14 -0300

    feat: prepare learning 02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+﻿
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResumeDetails()
+    {
+        Console.WriteLine("Name: {0}",_name);
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobInformation();
+        }
+    }
+}
