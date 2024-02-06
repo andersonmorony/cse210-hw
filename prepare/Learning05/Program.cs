f30a0de496b8d9b9766735ac5fb7a46c718d8ea6
Anderson Moroni <andersonmoroni92@gmail.com>
2024-02-05 21:14:05 -0300

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
+using Learning05;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 22.9),
+            new Rectangle("Blue", 15,7),
+            new Circle("White", 50)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} - Area: {shape.GetArea()}");
+        }
+
+
     }
 }
